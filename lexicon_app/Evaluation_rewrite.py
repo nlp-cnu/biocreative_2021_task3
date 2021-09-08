278a104ab310fd35992d5e02ebe6ae47e4679aca
henryst57 <samuel.henry@cnu.edu>
2021-09-08 11:38:50 -0400

    fixed test file

--- lexicon_app/Evaluation_rewrite.py
+++ lexicon_app/Evaluation_rewrite.py
@@ -1,4 +1,4 @@
-from lexicon_app.eval.Evaluation import score_task
+from eval.Evaluation import score_task
 import os
 """
 def score_task(pred_file, gold_file, out_file):
@@ -15,7 +15,8 @@ with open("score_log.txt", "rt") as file:
 
 #training as gold standard
 training_goldstd = os.path.join("gold_std", "BioCreative_TrainTask3.tsv")
-score_task(os.path.join("test", "training_as_dict", "No_Stopwords_or_Subwords", "Train3.tsv"), training_goldstd, "score_log.txt")
+pred = os.path.join("test", "training_as_dict", "No_Stopwords_or_Subwords", "Train3.tsv")
+score_task(pred, training_goldstd, "score_log.txt")
 # score_task(os.path.join("test", "both_as_dict", "Subwords_only", "Val3.tsv"), training_goldstd, "score_log.txt")
 # score_task(os.path.join("test", "both_as_dict", "Stopwords_only", "Val3.tsv"), training_goldstd, "score_log.txt")
 # score_task(os.path.join("test", "both_as_dict", "Stopwords_and_Subwords", "Val3.tsv"), training_goldstd, "score_log.txt")
